167461f703ac021b79977ed9d67246b75b1129db
SEPANU1717 <130642137+SEPANU1717@users.noreply.github.com>
2023-05-29 17:01:27 +0300

    Update 04_☎Contact.py

--- pages/04_☎Contact.py
+++ pages/04_☎Contact.py
@@ -10,28 +10,31 @@ def get_img_as_base64(image_path):
         base64_encoded = base64.b64encode(image_data).decode("utf-8")
     return base64_encoded
 
+background_image_path = "image/mmm.png"
 image_path = "image/mm.png"
 im = Image.open(image_path)
 im2 = Image.open("image/logoside.png")
 st.set_page_config(page_title="BastonedProject", page_icon=im)
 
-# Define the tabs
-tab1, tab2 = st.tabs(["-", "Contact Us"])
+background_image = Image.open(background_image_path)
 
-with tab1:
-    lottie_url = "https://assets3.lottiefiles.com/packages/lf20_HxqVQ4.json"
+page_bg_img = f"""
+    <style>
+    [data-testid="stAppViewContainer"] > .main {{
+        background-image: url("data:image/png;base64,{get_img_as_base64(background_image_path)}");
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center center;
+    }}
+    </style>
+    """
 
-    # Load the Lottie animation
-    response = requests.get(lottie_url)
-    lottie_json = response.json()
+st.markdown(page_bg_img, unsafe_allow_html=True)
 
-    # Render the Lottie animation in Streamlit
-    st_lottie(lottie_json)
 
-with tab2:
-    st.header("Get in touch with Us")
-    st.write("Please use the form below to send us a message:")
-    contact_form = """
+st.header("Get in touch with Us")
+st.write("Please use the form below to send us a message:")
+contact_form = """
         <form action="https://formsubmit.co/bastonedproject@gmail.com" 
               method="POST">
              <input type="hidden" name="_captcha" value="false">     
@@ -41,7 +44,7 @@ with tab2:
              <button type="submit">Send</button>
         </form>
         """
-    st.markdown(contact_form, unsafe_allow_html=True)
+st.markdown(contact_form, unsafe_allow_html=True)
 
 # CSS
 def local_css(file_name):
